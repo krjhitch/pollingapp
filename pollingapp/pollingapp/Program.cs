3da95c15de6b5ebd60143c001083afcbab83139b
Keith Hitchcock <keihi@microsoft.com>
2019-02-10 19:22:26 -0800

    initial class framework

---
+++ pollingapp/pollingapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pollingapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
